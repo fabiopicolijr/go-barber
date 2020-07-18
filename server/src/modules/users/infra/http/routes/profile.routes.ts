452d6c740ec17d230716a061c90670278b759598
Fabio Picoli Jr <fabiopicolijr@gmail.com>
2020-07-18 19:11:10 -0300

    validations with celebrate and Joi

--- server/src/modules/users/infra/http/routes/profile.routes.ts
+++ server/src/modules/users/infra/http/routes/profile.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 
@@ -10,6 +11,18 @@ const profileController = new ProfileController();
 profileRouter.use(ensureAuthenticated);
 
 profileRouter.put('/', profileController.update);
-profileRouter.get('/', profileController.show);
+profileRouter.get(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      old_password: Joi.string(),
+      password: Joi.string(), // daria para melhorar essa validacao colocando uma condicional, se o old estiver preenchio, colocar o new como obrigatorio
+      password_confirmation: Joi.string().required().valid(Joi.ref('password')),
+    },
+  }),
+  profileController.show,
+);
 
 export default profileRouter;
--- server/src/modules/users/infra/http/routes/users.routes.ts
+++ server/src/modules/users/infra/http/routes/users.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import multer from 'multer';
+import { celebrate, Segments, Joi } from 'celebrate';
 
 import uploadConfig from '@config/upload';
 
@@ -13,8 +14,19 @@ const upload = multer(uploadConfig);
 const usersController = new UsersController();
 const userAvatarController = new UserAvatarController();
 
-usersRouter.post('/', usersController.create);
+usersRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+    },
+  }),
+  usersController.create,
+);
 
+// o celebrate nao valida arquivos, por isso nao colocamos nada aqui.
 usersRouter.patch(
   '/avatar',
   ensureAuthenticated,
