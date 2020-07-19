57450ee60727be691ba9d2e4260a32e063b5fb54
Fabio Picoli Jr <fabiopicolijr@gmail.com>
2020-07-18 22:51:30 -0300

    class-transformer

--- server/src/modules/users/infra/http/controllers/UserAvatarController.ts
+++ server/src/modules/users/infra/http/controllers/UserAvatarController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
+import { classToClass } from 'class-transformer';
 
 import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService';
 
@@ -14,6 +15,6 @@ export default class UsersController {
 
     delete user.password;
 
-    return response.json(user);
+    return response.json({ user: classToClass(user) });
   }
 }
--- server/src/modules/users/infra/typeorm/entities/User.ts
+++ server/src/modules/users/infra/typeorm/entities/User.ts
@@ -7,6 +7,8 @@ import {
   UpdateDateColumn,
 } from 'typeorm';
 
+import { Expose, Exclude } from 'class-transformer';
+
 @Entity('users')
 class User {
   @PrimaryGeneratedColumn('uuid')
@@ -19,9 +21,11 @@ class User {
   email: string;
 
   @Column()
+  @Exclude()
   password: string;
 
   @Column()
+  @Exclude()
   avatar: string;
 
   @CreateDateColumn()
@@ -29,6 +33,13 @@ class User {
 
   @UpdateDateColumn()
   updated_at: Date;
+
+  @Expose({ name: 'avatar_url' })
+  getAvatarUrl(): string | null {
+    return this.avatar
+      ? `${process.env.APP_API_URL}/files/${this.avatar}`
+      : null;
+  }
 }
 
 export default User;
